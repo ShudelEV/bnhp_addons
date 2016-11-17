980f1245736ca30bfe8ac2eb00d7c62db695c2d5
ShudelEV <e.shudel@mail.ru>
2016-11-17 15:13:22 +0300

    [NEW]

---
+++ project_task_order/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+{
+    'name': 'Order by project sequence',
+    'version': '1.0.0',
+    'category': 'Projects & Services',
+    'sequence': 16,
+    'summary': '',
+    'author':  'ShEV',
+    'images': [
+    ],
+    'depends': [
+        'project',
+        'project_section',
+    ],
+    'data': [
+        'views/project_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
